1d8f6975d5c04cb231ebb7a65e1d8252a6964947
Anmol Tripathi <anmoltripathi610@gmail.com>
2025-02-27 04:34:27 +0530

    README add

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const useStore = create()(
+export const useStore = create(
   persist(
     (set) => ({
       selectedUser: null,
